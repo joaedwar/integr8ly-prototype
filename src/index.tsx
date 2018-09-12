a19f0b7ce0dfa43e0eb858c68e71bff32a70a7a0
Donald Labaj <dlabaj@redhat.com>
2018-09-12 16:03:26 -0400

    Initial modifications for typescript migration.

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { HashRouter as Router } from 'react-router-dom';
 import 'patternfly/dist/css/patternfly.css';
@@ -7,8 +7,8 @@ import 'patternfly/dist/css/patternfly-additions.css';
 import './styles/.css/index.css';
 
 import App from './App';
-import { baseName } from './routes';
 import store from './redux/store';
+import { baseName } from './routes';
 
 ReactDOM.render(
   <Provider store={store}>
